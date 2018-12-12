3d7f14fe42b3c7279bcc6b55962fd9713e8d30c9
seemal <schauhan1206@gmail.com>
2018-12-12 01:46:30 -0500

    commit push dowhileloop

---
+++ testloop/src/newloop/DoWhileLoop.java
@@ -0,0 +1,17 @@
+package newloop;
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		int i = 6;
+		do {
+		System.out.println(i);
+		i++;
+		
+
+	} while(i<12);
+		
+	}
+}
+
+
