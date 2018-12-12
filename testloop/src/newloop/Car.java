c0444cbbf7517c54abbc8c543bd18ddb39d4d3e9
seemal <schauhan1206@gmail.com>
2018-12-12 02:18:17 -0500

    Car loop commit and push

---
+++ testloop/src/newloop/Car.java
@@ -0,0 +1,12 @@
+package newloop;
+
+public class Car {
+
+	public Car(int i, String string) {
+		// TODO Auto-generated constructor stub
+	}
+	public static final int modelName = 0;
+	public String modelYear;
+	
+
+}
---
+++ testloop/src/newloop/CarLoop.java
@@ -0,0 +1,36 @@
+package newloop;
+
+public class CarLoop {
+
+	
+		
+			  int modelYear;
+			  String modelName;
+
+			  public void Car(int year, String name) {
+			    modelYear = year;
+			    modelName = name;}
+			  
+
+			    public static void main(String[] args) {
+
+			    Car myCar = new Car(1969, "Mustang");
+			    System.out.println(myCar.modelYear + " " + myCar.modelName);
+			  }
+			
+		
+		  
+			
+			
+			
+		
+
+
+				
+				
+				
+				
+
+	}
+
+
